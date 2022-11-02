390b3d832ecbd6765ef44f143778f319fada9199
Mohamad Dakdouk <mdak2806@gmail.com>
2022-11-02 11:52:00 +1100

    database set up

--- models/seeds.js
+++ models/seeds.js
@@ -1,2 +1,40 @@
 let db;
 const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.0.1:27017', { }, (err, client) => {
+    if(err){
+        console.log('error connecting to mongoDB server', err);
+        process.exit(1);
+    }
+    db = client.db('pb');
+    insertBlogs();
+
+});
+
+const insertBlogs = () => {
+
+    db.collection('blogs').insertMany([
+        {
+            title: 'My new blog post',
+            author: 'Mo',
+            content: 'Lorem functionality does not work',
+            img:  'http://fillmurray/200/200',
+        },
+        {
+            title: 'My new blog post',
+            author: 'Mo',
+            content: 'Lorem functionality does not work',
+            img:  'http://fillmurray/200/200',
+        },
+        {
+            title: 'My new blog post',
+            author: 'Mo',
+            content: 'Lorem functionality does not work',
+            img:  'http://fillmurray/200/200',
+        },
+    ])
+
+
+}
+
+
