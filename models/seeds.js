f6ff4b205b5140ed22d680d02968936f8ce0f443
Dee Pabst <dee@canva.com>
2022-11-02 12:08:09 +1100

    updated seeds file

--- models/seeds.js
+++ models/seeds.js
@@ -1,8 +1,8 @@
 let db;
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://127.0.0.0.1:27017', { }, (err, client) => {
-    if(err){
+MongoClient.connect('mongodb://127.0.0.1:27017', {}, (err, client) => {
+    if (err) {
         console.log('error connecting to mongoDB server', err);
         process.exit(1);
     }
@@ -18,21 +18,30 @@ const insertBlogs = () => {
             title: 'My new blog post',
             author: 'Mo',
             content: 'Lorem functionality does not work',
-            img:  'http://fillmurray/200/200',
+            img: 'https://www.fillmurray.com/200/200',
         },
         {
-            title: 'My new blog post',
-            author: 'Mo',
-            content: 'Lorem functionality does not work',
-            img:  'http://fillmurray/200/200',
+            title: 'life after SEI55',
+            author: 'Dee',
+            content: 'Backend engineering jokes',
+            img: 'https://www.fillmurray.com/200/200',
         },
         {
-            title: 'My new blog post',
-            author: 'Mo',
-            content: 'Lorem functionality does not work',
-            img:  'http://fillmurray/200/200',
+            title: 'Life in Hobart',
+            author: 'Wen',
+            content: 'Beautiful and boring',
+            img: 'https://www.fillmurray.com/200/200',
         },
-    ])
+    ],
+        (err, result) => {
+            if (err) {
+                console.log('Error inserting blog posts', err);
+                return;
+            }
+            console.log(`Success! added ${result.insertedCount} Blog posts`);
+            process.exit(0); // no errors
+        }
+    )
 
 
 }
