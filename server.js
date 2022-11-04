0b667563d65c009c5858826239e40b6696b32b31
Dee Pabst <dee@canva.com>
2022-11-04 11:07:11 +1100

    added db connection and first routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost/${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`)
 })
 
 const mongoose = require('mongoose')
@@ -14,8 +14,17 @@ const Blog = require('./models/Blog')
 mongoose.connect('mongodb://127.0.0.1/pb')
 
 const db = mongoose.connection
+db.on('error', err => {
+  console.log('Error connecting to server', err)
+  process.exit(1)
+})
 
 app.get('/', (req, res) => {
   console.log('Root route was requested')
   res.json({hello: 'there'})
+})
+
+app.get('/blogs', async (req, res) => {
+  const blogs = await Blog.find();
+  res.json(blogs);
 })
